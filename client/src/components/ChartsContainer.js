758c527c95c617044c42b7d81b31c969dcfaf587
Andrew Bernklau <andrewbernklau@hotmail.com>
2022-05-04 16:03:26 -0600

    Added monthly application charts and functionality

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,7 +1,26 @@
-import React from "react";
+import AreaChart from "./AreaChart";
+import BarChart from "./BarChart";
+import { useAppContext } from "../context/appContext";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import { useState } from "react";
 
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button
+        type="button"
+        onClick={() => {
+          setBarChart(!barChart);
+        }}
+      >
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
